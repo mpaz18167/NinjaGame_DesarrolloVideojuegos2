b6618019bc475ea53387ce60a18a99befeeddf9d
mpaz18167 <locociber2001@gmail.com>
2023-05-15 15:16:44 -0500

    Camera Controller
    Se Actualizo la camara a utilizar, se coloco primero una CinemaCamera, Se opto despues por un scirpt que usaria lo mismo pero con menos recursos

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Target;
+    private Vector3 TargetPos;
+
+    public float HaciaAdelante;
+    public float Smoothing;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        TargetPos = new Vector3(Target.transform.position.x,Target.transform.position.y, transform.position.z);
+
+        if (Target.transform.localScale.x ==1)//DERECHA
+        {
+            TargetPos = new Vector3(TargetPos.x + HaciaAdelante, TargetPos.y, transform.position.z);
+        }
+
+        if (Target.transform.localScale.x == -1)//IZQUIERDA
+        {
+            TargetPos = new Vector3(TargetPos.x - HaciaAdelante, TargetPos.y, transform.position.z);
+        }
+
+        transform.position = Vector3.Lerp(transform.position,TargetPos,Smoothing*Time.deltaTime);
+
+    }
+
+
+}
